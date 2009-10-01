52bc64fb52309a7dc79decc7bedbee8fe3b84002
bart vandendriessche <bart.vandendriessche@gmail.com>
2009-10-01 15:58:25 +0200

    comment out the paredit stuff for a while

--- starter-kit-js.el
+++ starter-kit-js.el
@@ -6,14 +6,14 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . espresso-mode))
 (add-hook 'espresso-mode-hook 'moz-minor-mode)
-(add-hook 'espresso-mode-hook 'esk-paredit-nonlisp)
+;;(add-hook 'espresso-mode-hook 'esk-paredit-nonlisp)
 (add-hook 'espresso-mode-hook 'run-coding-hook)
 (add-hook 'espresso-mode-hook 'idle-highlight)
 (setq espresso-indent-level 2)
 
 (eval-after-load 'espresso
-  '(progn (define-key espresso-mode-map "{" 'paredit-open-curly)
-          (define-key espresso-mode-map "}" 'paredit-close-curly-and-newline)
+  '(progn ;; (define-key espresso-mode-map "{" 'paredit-open-curly)
+          ;; (define-key espresso-mode-map "}" 'paredit-close-curly-and-newline)
           ;; fixes problem with pretty function font-lock
           (define-key espresso-mode-map (kbd ",") 'self-insert-command)
           (font-lock-add-keywords 'espresso-mode
